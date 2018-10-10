dee34128e31cca032be4eae23919fa8e73d51942
pal_user <amit.mahajan@hcl.com>
2018-10-10 11:08:27 -0600

    database migrations with jdbctemplate

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -31,8 +31,6 @@ public class TimeEntryController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(timeEntry, HttpStatus.OK);
-
-
     }
 
     @GetMapping
